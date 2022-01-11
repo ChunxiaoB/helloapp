3db1363e45ecd32a852f3b2805abb9795d8fab9d
Chunxiao Bi <chunxiao.bi@rcsb.org>
2022-01-11 12:36:39 -0800

    add delete function

--- flask_blog/app.py
+++ flask_blog/app.py
@@ -18,7 +18,6 @@ y = {"student_name":"Han Li",
 @app.route("/display_json")
 def display_json_in_table():
     data = json.load(open("data_source.json"))
-    #return render_template('display_json.html', title="DISPLAY JSON DATA IN HTML", jsondata=json.dumps(data))
     return render_template('display_json.html', title="DISPLAY JSON DATA IN HTML", jsondata=data["signup_details"])
 
 
@@ -40,9 +39,39 @@ def write_json(filename='data_source.json'):
         file.seek(0)
         # convert back to json.
         json.dump(file_data, file, indent = 4)
+        file.close()
+    # final step: display the json with new data in html
+    return render_template('display_json.html', title="DISPLAY JSON DATA IN HTML", jsondata=file_data["signup_details"])
 
-        # final step: display the json with new data in html
-        return render_template('display_json.html', title="DISPLAY JSON DATA IN HTML", jsondata=file_data["signup_details"])
+
+@app.route("/remove", methods = ['POST', 'GET'])
+# function to add to JSON
+# return display_json.html with new data
+# that contain
+def remove_element(filename='data_source.json'):
+    if request.method == 'POST':
+        remove_me = request.form
+        print(remove_me['student_name'])
+        print(remove_me['signup_date'])
+
+    with open(filename,'r+') as file:
+        # First we load existing data into a dict.
+        file_data = json.load(file)
+        # Join new_data with file_data inside emp_details
+
+        # file_data["signup_details"] = list(filter(lambda i: i['student_name'] != "Han", file_data["signup_details"]))
+        file_data["signup_details"] = list(filter(lambda i: ( (i['student_name'] != remove_me['student_name']) and (i['signup_date'] != remove_me['signup_date'])), file_data["signup_details"]))
+
+        # print(file_data)
+
+        # Sets file's current position at offset.
+        file.seek(0)
+        file.close()
+        # convert back to json.
+        json.dump(file_data, open(filename, 'w+'), indent = 4)
+
+    # final step: display the json with new data in html
+    return render_template('display_json.html', title="DISPLAY JSON DATA IN HTML", jsondata=file_data["signup_details"])
 
 
 @app.route("/")
